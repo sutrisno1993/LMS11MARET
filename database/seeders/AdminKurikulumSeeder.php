3bdb2557be7b5b8deb63555d267fe5370e8ebcf6
sutrisno1993 <sutrisno1993@users.noreply.github.com>
2026-06-27 20:22:33 +0700

    Add AdminKurikulumSeeder

---
+++ database/seeders/AdminKurikulumSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class AdminKurikulumSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Membuat atau memperbarui admin dengan email dan password yang diminta
+        User::updateOrCreate(
+            ['email' => 'admin2@smk11maret.sch.id'], 
+            [
+                'name' => 'Admin Kurikulum Super',
+                'password' => Hash::make('Sutrisno_123'),
+                'role' => 'ADMIN',
+            ]
+        );
+    }
+}
